c24839da55adce5d338b5cff46e7d8097bf609da
Jan Palma <honzik@janpalma.cz>
2025-06-18 08:11:28 +0200

    Mobile Version Implementation

--- assets/js/navbar-scroll.js
+++
@@ -1,9 +0,0 @@
-const homeNav = document.getElementById("homenav")
-
-window.addEventListener("scroll",(ev) => {
-    if (window.scrollY > 10) {
-        homeNav.classList.add("scroll")
-    } else {
-        homeNav.classList.remove("scroll")
-    }
-})
